f6810e6a0b4ab346c70c52f75f128b1070c3bdfd
Rodrigo Barbosa <rodrigo@aroeleven.com.br>
2025-07-11 10:32:58 -0300

    Atualizando BASE_URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from "../model/questao";
 import Questionario from "../components/Questionario";
 import { useRouter } from "next/router";
   
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://curso-next-quiz-ecru.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
